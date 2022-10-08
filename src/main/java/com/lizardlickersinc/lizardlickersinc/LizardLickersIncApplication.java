03f9a11030afdbee260536c479c0ff17172f3dec
Josh Gutowski <cecius1@gmail.com>
2022-10-08 13:26:56 -0400

    init commit

---
+++ src/main/java/com/lizardlickersinc/lizardlickersinc/LizardLickersIncApplication.java
@@ -0,0 +1,13 @@
+package com.lizardlickersinc.lizardlickersinc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LizardLickersIncApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LizardLickersIncApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/lizardlickersinc/lizardlickersinc/LizardLickersIncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lizardlickersinc.lizardlickersinc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LizardLickersIncApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
